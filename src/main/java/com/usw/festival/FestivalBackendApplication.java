f3bdd10852bc6d8470ce066958c9af90168902e7
kimhoeyun <clnvrd@suwon.ac.kr>
2026-04-06 13:05:55 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/usw/festival/FestivalBackendApplication.java
@@ -0,0 +1,12 @@
+package com.usw.festival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FestivalBackendApplication.class, args);
+    }
+}
